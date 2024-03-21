6f62aa6b3d6c79fc7f54db011985d939de9b664b
DiegoIraheta00041923 <00041923@uca.edu.sv>
2024-03-21 15:54:57 -0600

    Archivo Calculadora y archivo Operaciones

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Calculadora {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int x,y,opcion;
+    do{
+        System.out.println("Que desea hacer: \n1.Suma \n2.Resta \n3.Division");
+    }while();
+
+    }
+}
